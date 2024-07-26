9acf2ee85ac4f1e6f3ef820378b67c048a6493bc
HarrisonKanellos <harrisonkanellos@gmail.com>
2024-07-26 18:38:39 +1000

    Fix styling to allow for grid resizing, add class by classList

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let body = document.querySelector("body");
 
+let buttonContainer = document.createElement("div");
+buttonContainer.classList.add("button-container");
+
 let container = document.createElement("div");
-container.setAttribute("class", "grid-container");
+container.classList.add("grid-container");
 body.appendChild(container);
 
 // Create grid 16x16
 let k = 0;
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 20; i++) {
     let row = document.createElement("div");
-    row.setAttribute("class", "grid-row");
+    row.classList.add("grid-row");
     row.setAttribute("id", `row-${i+1}`);
     container.appendChild(row);
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < 20; j++) {
         let box = document.createElement("div");
-        box.setAttribute("class", "grid-box");
+        box.classList.add("grid-box");
         box.setAttribute("id", `box-${(i+1)*(j+1)}`);
         row.appendChild(box);
         k++;
